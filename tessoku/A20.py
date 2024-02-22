d8d756dfa2297979f47556914e9843afd37c5982
chikati3 <chikazawakazuhiro3@gmail.com>
2024-02-22 21:59:17 +0900

    A20

---
+++ tessoku/A20.py
@@ -0,0 +1,11 @@
+S = input()
+T = input()
+
+dp = [[0] * (len(T)+1) for _ in range(len(S)+1)]
+for i in range(len(S)):
+    for j in range(len(T)):
+        if S[i] == T[j]:
+            dp[i+1][j+1] = dp[i][j] + 1
+        else:
+            dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+print(dp[-1][-1])
